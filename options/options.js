3b6b825079047d4e008bac14024723a4e12ae243
Crowdbotics <engineering@crowdbotics.com>
2023-10-18 10:14:22 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "little-silence-dev-123210",
+  url: "https://little-silence-dev-123210.botics.co",
+  api: "https://little-silence-dev-123210.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
